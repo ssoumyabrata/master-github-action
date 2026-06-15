f0bc9f3a429fff6f0ef91f092d7a03d5bc2e5401
Soumyabrata Saha <soumyabrata.ceiindia@gmail.com>
2026-06-15 22:48:04 +0530

    updated testdir path

--- playwright.config.js
+++ playwright.config.js
@@ -1,7 +1,7 @@
 const { defineConfig, devices } = require('@playwright/test');
 
 module.exports = defineConfig({
-  testDir: './tests/Jun26',
+  testDir: './tests/',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
